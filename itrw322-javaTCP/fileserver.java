ecd105763fd40647a8f981f58be8ac923fd23e86
Pido <23745584@student.g.nwu.ac.za>
2018-10-15 23:03:01 +0200

    java/TCP/klaar

---
+++ itrw322-javaTCP/fileserver.java
@@ -0,0 +1,20 @@
+/*File Server****************************************************/
+
+import java.io.*;
+import java.net.*;
+
+public class fileserver {
+public static void main(String[] args) throws Exception {
+ServerSocket s=new ServerSocket(5643);
+Socket sr=s.accept();
+FileInputStream fr=new FileInputStream("C:/Users/User/Desktop/itrw322-javaTCP/server.txt");
+/* include your file name which you want to move*/
+byte b[]=new byte[2002];
+fr.read(b, 0, b.length);
+OutputStream os=sr.getOutputStream();
+os.write(b, 0, b.length);
+
+}
+}
+
+/************************************************************************/
