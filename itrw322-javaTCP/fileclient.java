40d7c361ae25d5983325d4e491607e2de2081b21
EdwardAdriaan <42216791+EdwardAdriaan@users.noreply.github.com>
2018-10-17 17:26:54 +0200

    chnages for TCP

--- itrw322-javaTCP/fileclient.java
+++ itrw322-javaTCP/fileclient.java
@@ -1,22 +1,23 @@
+
 /* File Client *********************************************************/
 
 import java.io.*;
 import java.net.*;
 import java.util.Timer;
 
+public class fileclient {
+    public static void main(String[] argv) throws Exception {
+        long startTime = System.currentTimeMillis();
+        long elapsedTime;
+        byte[] b = new byte[100000];
+        Socket sr = new Socket("192.168.1.7", 5643);
+        InputStream is = sr.getInputStream();
+        FileOutputStream fr = new FileOutputStream(
+                "C:/Users/EA/Desktop/EA/UTC,TCP repo/itrw-322-TCP-UDP-/itrw322-javaTCP/test.png");
+        is.read(b, 0, b.length);
+        fr.write(b, 0, b.length);
 
-public class fileclient{
-public static void main(String[] argv) throws Exception {
-    long startTime = System.currentTimeMillis();
-    long elapsedTime;
-byte []b=new byte[2002];
-Socket sr=new Socket("localhost",5643);
-InputStream is=sr.getInputStream();
-FileOutputStream fr=new FileOutputStream("C:/Users/User/Desktop/test/testing.txt");
-is.read(b,0,b.length);
-fr.write(b, 0, b.length);
-
-elapsedTime = System.currentTimeMillis() - startTime ;
-    System.out.println(elapsedTime);
-}
+        elapsedTime = System.currentTimeMillis() - startTime;
+        System.out.println(elapsedTime);
+    }
 }
